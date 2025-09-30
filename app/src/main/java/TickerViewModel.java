8be89f22bc7eb06969c26dc758657c302e180d7f
Jeremiah <jermcdon18@gmail.com>
2025-09-29 20:32:23 -0500

    Created TickerViewModel.java and methods for it.

---
+++ app/src/main/java/TickerViewModel.java
@@ -0,0 +1,54 @@
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.LinkedList;
+
+public class TickerViewModel extends ViewModel {
+    private static final int MAX = 6;
+
+    private MutableLiveData<LinkedList<String>> tickers;
+    private MutableLiveData<String> selected;
+
+    public TickerViewModel() {
+        tickers = new MutableLiveData<>(new LinkedList<>());
+        selected = new MutableLiveData<>(null);
+    }
+
+    public void defaultEntries(){
+        LinkedList<String> v = tickers.getValue();
+        if(v == null){
+            v = new LinkedList<>();
+        }
+        if(v.isEmpty()){
+            v.add("NEE");
+            v.add("AAPL");
+            v.add("DIS");
+        }
+        tickers.setValue(v);
+    }
+
+    public MutableLiveData<LinkedList<String>> getTickers() {
+        return tickers;
+    }
+
+    public MutableLiveData<String> getSelected() {
+        return selected;
+    }
+
+    public void addTicker(String str){
+        if(str == null || str.isEmpty()){
+            return;
+        }
+
+        LinkedList<String> v = new LinkedList<>();
+        v.remove(str);
+
+        if(v.size() < MAX){
+            v.add(str);
+        } else{
+            v.set(MAX - 1, str);
+        }
+        tickers.setValue(v);
+
+    }
+}
